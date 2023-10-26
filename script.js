eb5f8f349a602c1d4e7f73f4eeec9209d8896a69
AHMED HOSSAM <124696607+AhmedHossam911@users.noreply.github.com>
2023-10-26 18:38:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ document.getElementById("student-form").addEventListener("submit", function (e) 
 
   const enteredID = parseInt(document.getElementsByName("username")[0].value);
 
-  // Make an AJAX request to load the JSON data
-  fetch("DataBase1.json")
-      .then(response => response.json())
-      .then(data => {
-          const student = data.find(studentData => parseInt(studentData["Student ID number"]) === enteredID);
+  // Use the raw GitHub URL for your JSON file
+  const githubRawURL = "https://raw.githubusercontent.com/AhmedHossam911/test-reg/main/DataBase1.json";
 
-          if (student) {
-              // Check if it's the right time to register here and set the message accordingly
-              const registrationTimeIsRight = true; // Change this condition based on your logic
-              if (registrationTimeIsRight) {
-                  window.location.href = "https://www.facebook.com/"; // Replace with your desired URL
-              } else {
-                  document.getElementById("message").textContent = "Sorry, registration time has not come yet.";
-              }
-          } else {
-              document.getElementById("message").textContent = "Wrong ID. Please check it again.";
-          }
-      })
-      .catch(error => console.error("Error loading data: " + error));
+  // Make an AJAX request to load the JSON data
+  fetch(githubRawURL)
+    .then(response => response.json())
+    .then(data => {
+      const student = data.find(studentData => studentData["Student ID number"] === enteredID);
+
+      if (student) {
+        // Check if it's the right time to register here and set the message accordingly
+        const registrationTimeIsRight = true; // Change this condition based on your logic
+        if (registrationTimeIsRight) {
+          window.location.href = "https://www.facebook.com/";
+        } else {
+          document.getElementById("message").textContent = "Sorry, registration time has not come yet.";
+        }
+      } else {
+        document.getElementById("message").textContent = "Wrong ID. Please check it again.";
+      }
+    })
+    .catch(error => console.error("Error loading data: " + error));
 });
